ac29b2bc175e7bb544c55563e84df2d4d611814d
Daniel <olgierd.dan@gmail.com>
2019-03-10 11:30:28 +0100

    Updates

---
+++ precache-manifest.8e6105731a9af025f86edbc47f197e63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3e1419df2a1ac00aadeb8d1b62fe4c7",
+    "url": "/static/media/beep.f3e1419d.mp3"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "156d1961ac5d520466d6",
+    "url": "/static/js/main.156d1961.chunk.js"
+  },
+  {
+    "revision": "15bee817415565bcc017",
+    "url": "/static/js/1.15bee817.chunk.js"
+  },
+  {
+    "revision": "156d1961ac5d520466d6",
+    "url": "/static/css/main.e2c56c6a.chunk.css"
+  },
+  {
+    "revision": "64f77af6ff2be4d98188b94910b0354f",
+    "url": "/index.html"
+  }
+];
